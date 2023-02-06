db5f52c0cd3957419b442b496560d82792db2266
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-02-06 18:34:26 -0500

    initial commit

---
+++ UI/src/App.js
@@ -0,0 +1,239 @@
+function App() {
+  return (
+    <div className="App">
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lemon&family=Montserrat&display=swap"
+        rel="stylesheet"
+      />
+      <link href="https://fonts.googleapis.com/css2?family=Lemon&display=swap" rel="stylesheet" />
+      {/* <img
+        alt="another logo"
+        id="mustache"
+        src="https://i.pinimg.com/originals/16/7e/c9/167ec948cfe386a2f1fd8593d4a84a39.jpg"
+      />
+
+      <img
+        alt="another different logo"
+        id="mustache"
+        src="https://img1.pnghut.com/16/7/24/4HFsEwVFqK/clothing-accessories-smile-monochrome-black-monocle.jpg"
+      /> */}
+
+      <header id="header">
+        <div class="head-container">
+          <img
+            alt="monocle logo"
+            id="header-img"
+            src="https://t4.ftcdn.net/jpg/03/08/52/97/240_F_308529751_3Nc7sXda23z4EG53btAK2GDtoWy1dQwX.jpg"
+          />
+          <span>Monoclear</span>
+        </div>
+
+        <div id="head-nav">
+          <nav id="nav-bar" class="primary">
+            <ul>
+              <li>
+                <a class="nav-link" href="#about-us">
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a class="nav-link" href="#styles">
+                  Features
+                </a>
+              </li>
+              <li>
+                <a class="nav-link" href="#monocle-history">
+                  Monocle History
+                </a>
+              </li>
+              <li>
+                <a class="nav-link" href="#testimonials">
+                  Testimonials
+                </a>
+              </li>
+              <li>
+                <a class="nav-link" href="#contact-us">
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main>
+        <h1>Animonocles</h1>
+        <p class="tag">Enhancing Animal Lives, One Eyeball at a Time</p>
+
+        <img
+          id="line"
+          alt="monocle-lineup"
+          src="https://t4.ftcdn.net/jpg/03/67/19/21/240_F_367192146_9xPUw2OrsNkBP191vzWShSO4kH2z7ND6.jpg"
+        />
+        <section class="block">
+          <h2 id="about-us">About Us</h2>
+          <p class="hx">
+            Monocle ipsum dolor sit amet craftsmanship artisanal Washlet delightful. Zürich the best
+            premium Muji Baggu Comme des Garçons. Punctual Toto Baggu smart Singapore remarkable
+            wardrobe Zürich boulevard impeccable iconic Gaggenau. Premium Singapore Lufthansa ryokan
+            soft power elegant boutique classic alluring Baggu sleepy liveable.
+          </p>
+          <p class="hx">
+            Handsome ryokan artisanal Beams, Fast Lane Sunspel Washlet exquisite charming Singapore
+            carefully curated extraordinary. Emerging classic Sunspel wardrobe, signature Marylebone
+            hand-crafted the best Baggu Singapore destination extraordinary smart the highest
+            quality alluring. Alluring first-class elegant charming sophisticated, hub global Muji
+            K-pop signature flat white premium international joy perfect.
+          </p>
+          <p class="hx">
+            Craftsmanship conversation elegant, eclectic Porter Lufthansa cosy. Exclusive intricate
+            Sunspel izakaya, Muji espresso punctual sharp flat white Comme des Garçons hub. Tote bag
+            conversation bulletin Zürich business class Shinkansen.
+          </p>
+        </section>
+        <h2 id="styles">Features</h2>
+
+        <div class="first-row">
+          <div class="feature-items">
+            <img
+              class="animalmono"
+              id="cat"
+              alt="cat-with-monocle"
+              src="https://t3.ftcdn.net/jpg/02/07/25/00/240_F_207250092_1mrFKBARoP5MsuS1Mz4w7XqGqpD2rjpj.jpg"
+            />
+            <input type="button" class="feature-button" value="Styles" />
+          </div>
+
+          <div class="feature-items">
+            <img
+              class="animalmono"
+              id="corgi"
+              alt="corgi-with-monocle"
+              src="https://t3.ftcdn.net/jpg/01/93/10/38/240_F_193103843_e4ZQofpz3MpzywsAebR5yTDSRMQ9sZLM.jpg"
+            />
+            <input type="button" class="feature-button" value="Sizes" />
+          </div>
+
+          <div class="feature-items">
+            <img
+              class="animalmono"
+              id="racoon"
+              alt="racoon-with-monocle"
+              src="https://t4.ftcdn.net/jpg/02/10/70/91/240_F_210709180_NTb9i0CF6vF5I9IhsaY7tPDiKHfRJHlW.jpg"
+            />
+            <input type="button" class="feature-button" value="Lenses" />
+          </div>
+        </div>
+
+        <div class="second-row">
+          <div class="feature-items">
+            <img
+              class="animalmono"
+              id="bat"
+              alt="bat-with-monocle"
+              src="https://t4.ftcdn.net/jpg/01/86/98/73/240_F_186987302_2yN57LeCqoahPeSIfH4vHYntdhYQEpm6.jpg"
+            />
+            <input type="button" class="feature-button" value="Prescription" />
+          </div>
+
+          <div class="feature-items">
+            <img
+              class="animalmono"
+              alt="penguin-with-monocle"
+              src="https://t4.ftcdn.net/jpg/01/97/98/41/240_F_197984141_ADIDpK04N8WRRyddxsgVfCeCBNJBZOGa.jpg"
+            />
+            <input type="button" class="feature-button" value="Accessories" />
+          </div>
+        </div>
+        <section class="block">
+          <h2 id="monocle-history">Monocle History</h2>
+
+          <div class="first-row">
+            <iframe
+              id="video"
+              title="youtube video link"
+              src="https://www.youtube.com/embed/yUjEw6-WjQo?start=0"
+              width="560"
+              height="315"
+            ></iframe>
+          </div>
+        </section>
+        <h2 id="testimonials">Testimonials</h2>
+
+        <div class="first-row">
+          <div class="testimonials">
+            <img
+              class="testpic"
+              id="poodle"
+              alt="Huckleberry-with-monocle"
+              src="https://i.imgur.com/5yUC9.jpg"
+            />
+            <h3>Huckleberry</h3>
+            <p>
+              Craftsmanship conversation elegant, eclectic Porter Lufthansa cosy. Exclusive
+              intricate Sunspel izakaya, Muji espresso punctual sharp flat white Comme des Garçons
+              hub. Tote bag conversation bulletin Zürich business class Shinkansen.{' '}
+            </p>
+          </div>
+
+          <div class="testimonials">
+            <img
+              class="testpic"
+              id="real-cat"
+              alt="pierre-with-monocle"
+              src="https://i.imgflip.com/2rs7px.png"
+            />
+            <h3>Pierre</h3>
+            <p>
+              Craftsmanship conversation elegant, eclectic Porter Lufthansa cosy. Exclusive
+              intricate Sunspel izakaya, Muji espresso punctual sharp flat white Comme des Garçons
+              hub. Tote bag conversation bulletin Zürich business class Shinkansen.{' '}
+            </p>
+          </div>
+
+          <div class="testimonials">
+            <img
+              class="testpic"
+              id="pug"
+              alt="flora-with-monocle"
+              src="https://laughingsquid.com/wp-content/uploads/2014/03/bg_dogoclesss.jpg"
+            />
+            <h3>Flora</h3>
+            <p>
+              Craftsmanship conversation elegant, eclectic Porter Lufthansa cosy. Exclusive
+              intricate Sunspel izakaya, Muji espresso punctual sharp flat white Comme des Garçons
+              hub. Tote bag conversation bulletin Zürich business class Shinkansen.{' '}
+            </p>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <nav id="nav2nd">
+          <ul class="secondary">
+            <li>
+              <p>Find out about our return policy and warrenties for all our products. </p>
+              <a class="nav-link" href="#">
+                Returns & Guarantees
+              </a>
+            </li>
+            <li>
+              <p>
+                Please email help@petmonocles.com for assistance with orders, questions, or other
+                inquiries
+              </p>
+              <a class="nav-link" id="contact-us" href="#">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <form id="form" action="https://www.freecodecamp.com/email-submit">
+          <p>Join our monthly newsletter to monocle ipsum dolor sit amet</p>
+          <input name="email" type="email" id="email" placeholder="Email" required />
+          <input type="submit" id="submit" value="Sign Me Up" />
+        </form>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
